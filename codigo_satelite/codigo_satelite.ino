5135cde89a008521fc641e0ef8fb8e091f39db68
lockpicker <vigomga1993@gmail.com>
2020-01-23 21:11:37 +0100

    Reorganización código, creación módulo de drivers y módulo de protocolo ( separados ), implementado un modo debug

--- codigo_satelite/codigo_satelite.ino
+++ codigo_satelite/codigo_satelite.ino
@@ -1,11 +1,20 @@
-#include "TLE.hpp"
+#define TLE_DEBUG		1	// Si no se va a debuggear, poner a 0
+#include "TLE_protocol.hpp"
 
-TLE asd;
+/*
+ To do:
+ - Los datos constantes, moverlos en la EEPROM ( nombre del módulo ). ¿Qué cosas mover?
+ - 
+ - 
+ - 
+ */
+
+TLE asd; // temporal
+
 void setup()
 {
-  // temporal, mirar que tot tira
-  asd.algo();
-  Serial.print( sizeof(asd.TLE_buffer[0]) );
+  asd.init();	// temporal, mirar que todo tira
+  //Serial.print( sizeof(asd.TLE_buffer[0]) );
 
 
 }
--- codigo_satelite/drivers.h
+++
@@ -1,6 +0,0 @@
-#ifndef DRIVERS_H_	// Evitamos que se añada dos veces el mismo código
-#define DRIVERS_H_
-
-	#define PIN_PWM 6
-
-#endif
