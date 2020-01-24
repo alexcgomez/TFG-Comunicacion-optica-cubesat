7494c9ec66e9263edcddad8f72e1db0c8974fb91
lockpicker <vigomga1993@gmail.com>
2020-01-24 11:02:07 +0100

    Modificiación de la rutina de tiempo, falta probar con el osciloscopio

--- codigo_satelite/PWM_driver.hpp
+++ codigo_satelite/PWM_driver.hpp
@@ -6,8 +6,10 @@
 	//#define PIN_RECEPTOR_LASER	x // usar pin con IRQ por HW (pin 2?)
 	#define PIN_EMISOR_LASER 		6
 		#define LASER_OUTPUT()		pinMode( PIN_EMISOR_LASER, OUTPUT )
-		#define LASER_HIGH()		digitalWrite( PIN_EMISOR_LASER, HIGH)
-		#define LASER_LOW()			digitalWrite( PIN_EMISOR_LASER, LOW)
+		//define LASER_HIGH()		digitalWrite( PIN_EMISOR_LASER, HIGH)
+		#define LASER_HIGH()		_temporal_estado_laser = HIGH; digitalWrite( PIN_EMISOR_LASER, _temporal_estado_laser)
+		//#define LASER_LOW()		digitalWrite( PIN_EMISOR_LASER, LOW)
+		#define LASER_LOW()			_temporal_estado_laser = LOW; digitalWrite( PIN_EMISOR_LASER, _temporal_estado_laser)
 
 // Macros timing
 	#define NO_PRESCALING()		TCCR0B &= ~((1 << CS02) | (1 << CS01));	TCCR0B |= (1 << CS00)
@@ -20,29 +22,44 @@
 	#define DISABLE_TIMER_IRQ()	TIMSK0 &= ~(1 << OCIE0B)
 
 	#define RESET_TIMER()		TCNT0 = 0
+	#define TIMER				TCNT0
+	#define USER_COUNTER		OCR0B
+	
 
-
-	class PWM_driver
+//Variables
+	extern volatile unsigned char _temporal_estado_laser;
+	extern bool _new_packet;
+	extern bool _send_ready;
+	
+// Funciones
+	void _driver_init();
+	void _driver_write( char *_text, int text_size );
+	void _driver_read( char *_text, int text_size ); // No devolver el string con un return, suele dar problemas con el stack
+	
+	inline void _driver_send_byte( unsigned char dato ) // función inline, para ganar un poco de velocidad
 	{
-		public:
-			void init();
-			void write( char *_text, int text_size );
-			void read( char *_text, int text_size ); // No devolver el string con un return, suele dar problemas con el stack
-			bool is_sent();
-			bool new_packet();
-			
-			inline void send_byte( unsigned char dato ) // función inline, para ganar un poco de velocidad
-			{
-				ENABLE_TIMER_IRQ();
-				OCR0B = dato;
-				LASER_HIGH();
-			}
+		RESET_TIMER();
+		ENABLE_TIMER_IRQ();
+		USER_COUNTER = dato;
+		LASER_HIGH();
+	}
+	
+	inline bool _driver_can_send()
+	{
+		return _send_ready;
+	}
+	inline bool _driver_can_send(bool t)
+	{
+		_send_ready = t;
+	}
+	
 
-		private:
-			bool _new_packet = false; // modo read
-			bool _send_ready = false; // modo write
+	inline bool new_packet()
+	{
+		return _new_packet;
+	}
+
+
 			
-			
-	};
 
 #endif
