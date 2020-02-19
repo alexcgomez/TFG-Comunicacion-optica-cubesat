25f81f2c34833f9164c2da3cc2cf6857ba29aadf
lockpicker <vigomga1993@gmail.com>
2020-02-19 20:39:40 +0100

    autogenerado

---
+++ codigo_satelite/Core/Inc/transmisor.h
@@ -0,0 +1,14 @@
+#ifndef _transmisor_h_
+#define _transmisor_h_
+
+// Macros
+	#define FREE	0
+	#define SENDING	1
+
+// variables
+	extern volatile unsigned char _dato_a_enviar;
+	extern unsigned char can_send;
+	
+	void send_byte(unsigned char dada);
+
+#endif
