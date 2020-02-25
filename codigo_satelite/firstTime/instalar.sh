e7905760a3a028717746c2716444a65ffe697fe2
lockpicker <vigomga1993@gmail.com>
2020-02-26 00:03:31 +0100

    añadida la función para compilar e instalar el openocd más nuevo

--- codigo_satelite/firstTime/instalar.sh
+++ codigo_satelite/firstTime/instalar.sh
@@ -1,4 +1,25 @@
+#Permisos USB
 sudo usermod -a -G dialout $USER
 sudo cp ./*.rules /etc/udev/rules.d/
 
 sudo udevadm control --reload-rules
+
+
+#Librerías necesarias OpenOCD
+sudo apt -y install make
+sudo apt -y install libtool
+sudo apt -y install pkg-config
+sudo apt -y install libusb-0.1
+sudo apt -y install aclocal
+sudo apt -y install autotools-dev
+sudo apt -y install automake
+
+#Open OCD
+sudo apt -y purge openocd
+
+git clone https://github.com/ntfreak/openocd.git openocd
+cd openocd
+./bootstrap
+./configure
+make
+sudo make install
