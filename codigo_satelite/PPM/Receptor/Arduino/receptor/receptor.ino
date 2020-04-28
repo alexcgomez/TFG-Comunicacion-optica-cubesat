1ee5aebd7c06499334671a703198ee67ffd2eea9
lockpicker <vigomga1993@gmail.com>
2020-04-28 03:17:38 +0200

    probando modo sin pin trigger, todavía no va perfecto pero ya va mostrando avances

--- codigo_satelite/PPM/Receptor/Arduino/receptor/receptor.ino
+++ codigo_satelite/PPM/Receptor/Arduino/receptor/receptor.ino
@@ -1,6 +1,6 @@
 /*
   Notas importantes:
-  
+
   Arduino Uno, al ir a 16MHz, los micros no los lleva muy bien,
   así que la resolución de arduino es de múltiplos de 4
   ( vamos, que salta de 4 en 4 ), hay que tenerlo en cuenta para ajustar
@@ -10,46 +10,83 @@
 // ARM, PB12 (PPM)      => Arduino, 3 (PPM)
 // ARM, PB13 (Trigger)  => Arduino, 2 (IRQ In)
 
-#define PIN_IRQ 2
+#define BIT_STOP 4
 #define PIN_PPM 3
 
-volatile unsigned long tiempo = 0;
-
 #define SIZE_BUFFER 4 // relacionado con el nº de paquetes que envía el ARM
+
+volatile unsigned long tiempo = 0;
 byte puntero = 0;
 unsigned long paquetes[SIZE_BUFFER];
+bool bit_start = true;
+bool EOT = false;
 
 void setup()
 {
   Serial.begin(9600);
-  pinMode(PIN_IRQ, INPUT_PULLUP);
+  //pinMode(PIN_IRQ, INPUT_PULLUP);
   pinMode(PIN_PPM, INPUT);
-  attachInterrupt(digitalPinToInterrupt(PIN_IRQ), ISR_pin_IRQ, FALLING); // high=>low = ISR IRQ
-  attachInterrupt(digitalPinToInterrupt(PIN_PPM), ISR_pin_PPM, RISING); // low=>high = ISR PPM
+  //attachInterrupt(digitalPinToInterrupt(PIN_IRQ), ISR_pin_IRQ, FALLING); // high=>low = ISR IRQ
+  attachInterrupt(digitalPinToInterrupt(PIN_PPM), ISR_pin_PPM, CHANGE); // cambio estado pin = ISR PPM
 }
 
 void loop()
 {
-  if(puntero == SIZE_BUFFER )
+  if (EOT == true)
   {
     Serial.println("Datos captados");
-    
-    for(int i = 0; i < SIZE_BUFFER; i++)
+
+    for (int i = 0; i < SIZE_BUFFER; i++)
     {
       Serial.print((char)paquetes[i]);
     }
+    Serial.println("");
     puntero = (SIZE_BUFFER + 1); // para evitar que se imprima todo el rato
+    EOT = false;
   }
 }
 
 void ISR_pin_IRQ()
 {
-  tiempo = micros();
 }
-
 
 void ISR_pin_PPM()
 {
-  paquetes[puntero] = ((micros() - tiempo) / 132 );
-  puntero++;
+  // si es la caída del clock ( final del
+  // "byte"), nos interesa empezar de nuevo
+  // ya que es cuando empieza un nuevo paquete.
+
+  // Cuando esté en la subida, es que ya terminó
+  // el antiguo y cronometramos el tiempo pasado
+
+  if ( bit_start ) // sólo se usa para iniciar la trama
+  {
+    bit_start = false;
+  }
+  else
+  {
+    if ( digitalRead(PIN_PPM) == LOW )
+    {
+      tiempo = micros();
+    }
+    else // HIGH
+    {
+      char palabra = ((micros() - tiempo) / 133 );
+
+      if (palabra < BIT_STOP ) // señal de stop
+      {
+        EOT = true; // end of transmision
+        bit_start = true; // reset start para la próxima irq
+        puntero = 0;
+      }
+      else // byte normal
+      {
+        paquetes[puntero] = palabra;
+        puntero++;
+      }
+    }
+  }
 }
+
+
+
