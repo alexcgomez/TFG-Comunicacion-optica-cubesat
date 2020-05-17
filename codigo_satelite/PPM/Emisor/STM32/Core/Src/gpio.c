77a36478460f3e387df32e65ecc3549940cbf535
lockpicker <vigomga1993@gmail.com>
2020-05-17 16:03:37 +0200

    autogenerado

--- codigo_satelite/PPM/Emisor/STM32/Core/Src/gpio.c
+++ codigo_satelite/PPM/Emisor/STM32/Core/Src/gpio.c
@@ -50,22 +50,12 @@ void MX_GPIO_Init(void)
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(PPM_pin_GPIO_Port, PPM_pin_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(Trigger_pin_GPIO_Port, Trigger_pin_Pin, GPIO_PIN_SET);
-
   /*Configure GPIO pin : PtPin */
   GPIO_InitStruct.Pin = PPM_pin_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(PPM_pin_GPIO_Port, &GPIO_InitStruct);
-
-  /*Configure GPIO pin : PtPin */
-  GPIO_InitStruct.Pin = Trigger_pin_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_PULLUP;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(Trigger_pin_GPIO_Port, &GPIO_InitStruct);
 
 }
 
